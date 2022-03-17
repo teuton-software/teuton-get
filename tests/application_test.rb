ae1f1b395c55a1238535ccb2afbdaa47080238ca
David Vargas <dvarrui@protonmail.com>
2022-03-17 07:02:38 +0000

    [F] .

--- tests/application_test.rb
+++ tests/application_test.rb
@@ -6,6 +6,7 @@ require_relative '../lib/teuton-get/application'
 class ApplicationTest < Minitest::Test
   def setup
     @app = Application.instance
+    @config_path = File.join('/', 'home', 'david', '.config', 'teuton')
   end
 
   def test_application_env
@@ -13,21 +14,17 @@ class ApplicationTest < Minitest::Test
     assert_equal dirpath, @app.get('HOME')
   end
 
-  def test_param_config_dirpath
-    config_dirpath = File.join('/', 'home', 'david', '.teuton')
-    assert_equal config_dirpath, @app.get(:config_dirpath)
+  def test_param_config_path
+    assert_equal @config_path, @app.get(:config_dirpath)
   end
 
   def test_param_config_filepath
-    config_dirpath = File.join('/', 'home', 'david', '.teuton')
-    config_filepath = File.join(config_dirpath, 'repos.ini')
+    config_filepath = File.join(@config_path, 'repos.ini')
     assert_equal config_filepath, @app.get(:config_filepath)
-    assert_equal '/home/david/.teuton/cache', @app.get(:cache_dirpath)
   end
 
   def test_param_cache_dirpath
-    config_dirpath = File.join('/', 'home', 'david', '.teuton')
-    cache_dirpath = File.join(config_dirpath, 'cache')
+    cache_dirpath = File.join(@config_path, 'cache')
     assert_equal cache_dirpath, @app.get(:cache_dirpath)
   end
 end
