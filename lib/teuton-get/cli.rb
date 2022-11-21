01450164388cdefb96a1fecb728dc9717a08f36f
dvarrui <dvarrui@protonmail.com>
2022-11-21 18:50:59 +0000

    [U] .

--- lib/teuton-get/cli.rb
+++ lib/teuton-get/cli.rb
@@ -83,7 +83,7 @@ class CLI < Thor
 
   map ["d", "-d", "--download", "clone", "--clone"] => "download"
   desc "download TESTID", "Download Teuton test"
-  option :dir
+  option :dirname, type: :string
   long_desc <<-LONGDESC
     Download Teuton test. Example: "teutonget download teuton.en:systems.1/02-opensuse-conf"
   LONGDESC
