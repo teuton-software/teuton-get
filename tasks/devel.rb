334b671a05d2c7d63bba417f25c97b133055e7e1
David Vargas <dvarrui@protonmail.com>
2022-12-09 12:45:26 +0000

    [U] .

--- tasks/devel.rb
+++ tasks/devel.rb
@@ -4,7 +4,8 @@ namespace :devel do
     if File.exist? "/usr/local/bin/teutonget"
       puts "[WARN] Exist file /usr/local/bin/teutonget!"
     end
-    puts "[INFO] Creating launcher into /usr/local/bin"
+    puts "[INFO] Creating launcher 'teutonget' into /usr/local/bin"
     system("sudo cp tasks/files/teutonget /usr/local/bin/teutonget")
+    system("sudo ln -s /usr/local/bin/teutonget /usr/local/bin/teuton-get")
   end
 end
